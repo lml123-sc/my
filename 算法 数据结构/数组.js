1bae091af08decb7bb42797b8d411559f4615a9d
@lml <lml@zjtjyj.com>
2024-09-18 11:03:45 +0800

    feat:新增算法

--- 算法 数据结构/数组.js
+++ 算法 数据结构/数组.js
@@ -185,7 +185,7 @@ var rotate = function (nums, k) {
 var lengthOfLastWord = function (s) {
   let count = 0;
   let foundWord = false;
-  for (let i = s.length -1; i >= 0 ; i--) {
+  for (let i = s.length - 1; i >= 0; i--) {
     if (s[i] === " ") {
       if (foundWord) break;
     } else {
@@ -223,15 +223,82 @@ var lengthOfLastWord = function (s) {
 输出：0
 解释：在这种情况下, 交易无法获得正利润，所以不参与交易可以获得最大利润，最大利润为 0。
 */
-var maxProfit = function(prices) {
-  let a =0
-  for(let i=1;i<prices.length;i++) {
-    if(prices[i] > prices[i-1]) {
-      a+=prices[i] - prices[i-1];
+var maxProfit = function (prices) {
+  let a = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      a += prices[i] - prices[i - 1];
     }
   }
-  return a
+  return a;
 };
+/**
+* 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
 
-const arr = [7,1,5,3,6,4];
-console.log("lengthOfLastWord(arr)", maxProfit(arr));
+字符          数值
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+例如， 罗马数字 2 写做 II ，即为两个并列的 1 。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给定一个罗马数字，将其转换成整数。
+
+ 
+
+示例 1:
+
+输入: s = "III"
+输出: 3
+示例 2:
+
+输入: s = "IV"
+输出: 4
+示例 3:
+
+输入: s = "IX"
+输出: 9
+示例 4:
+
+输入: s = "LVIII"
+输出: 58
+解释: L = 50, V= 5, III = 3.
+示例 5:
+
+输入: s = "MCMXCIV"
+输出: 1994
+解释: M = 1000, CM = 900, XC = 90, IV = 4.
+*/
+var romanToInt = function (s) {
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const currentValue = romanMap[s[i]];
+    let prevValue = romanMap[s[i + 1]];
+
+    if (currentValue < prevValue) {
+      result -= currentValue;
+    } else {
+      result += currentValue;
+    }
+  }
+  return result;
+};
+const arr = "III"; // 58
+console.log("lengthOfLastWord(arr)", romanToInt(arr));
